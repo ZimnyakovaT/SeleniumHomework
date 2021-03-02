f3105b3eb3325da6dc39b7770001436fcdaa3786
ZimnyakovaT <79152588+ZimnyakovaT@users.noreply.github.com>
2021-03-02 21:32:12 +0700

    Задание 10. Проверить, что открывается правильная страница товара

--- Homework1/Homework1/Test3.cs
+++ Homework1/Homework1/Test3.cs
@@ -9,6 +9,7 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.IE;
 using NUnit.Framework;
+using System.Globalization;
 
 namespace Homework1
 {
@@ -45,8 +46,8 @@ namespace Homework1
             var duckscount = ducks.Count();
             int stickercount = 0;
             foreach (var duck in ducks)
-            { 
-              var sticker = duck.FindElements(By.CssSelector("[class*=sticker]"));
+            {
+                var sticker = duck.FindElements(By.CssSelector("[class*=sticker]"));
                 if (sticker.Count() == 1)
                 {
                     stickercount++;
@@ -57,7 +58,7 @@ namespace Homework1
             Assert.AreEqual(duckscount, stickercount);
         }
 
-        
+
 
         public void login()
         {
@@ -98,7 +99,7 @@ namespace Homework1
             CheckSortAsc(countryNames);
 
             //task 9 part 1.b
-            for (var j=0; j< countries.Count();j++)
+            for (var j = 0; j < countries.Count(); j++)
             {
                 var t1 = countries[j].FindElements(By.CssSelector("td")).ToArray();
                 if (t1[5].GetAttribute("textContent") != "0")
@@ -138,18 +139,18 @@ namespace Homework1
                 List<string> geoZones = new List<string>();
                 countries[j].FindElement(By.CssSelector("a")).Click();
                 wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.CssSelector("h1"))));
-                
+
                 var zones = driver.FindElement(By.Id("table-zones")).FindElements(By.CssSelector("tr"));
                 foreach (var zone in zones)
                 {
                     var tds = zone.FindElements(By.CssSelector("td"));
-                    if(tds.Count > 2)
+                    if (tds.Count > 2)
                     {
                         var options = tds[2]
                             .FindElement(By.CssSelector("select"))
                             .FindElements(By.CssSelector("option"));
 
-                        foreach(var option in options)
+                        foreach (var option in options)
                         {
                             if (option.Selected)
                                 geoZones.Add(option.Text);
@@ -162,6 +163,81 @@ namespace Homework1
 
                 driver.Navigate().Back();
                 countries = driver.FindElements(By.CssSelector(".row"));
+            }
+        }
+
+        [Test, Order(6)]
+        public void Test_6()
+        {
+            driver.Url = "http://localhost/litecart/";
+            var ducks = driver.FindElements(By.CssSelector("li a.link"));
+            for (var j = 0; j < ducks.Count(); j++)
+            {
+                var duckname = ducks[j].GetProperty("title").ToString();
+                var allprice = ducks[j].FindElement(By.CssSelector(".price-wrapper")).GetProperty("innerText").ToString();
+                ducks[j].Click();
+                wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.CssSelector("h1"))));
+                var header = driver.FindElement(By.CssSelector("h1")).GetProperty("textContent");
+                var allprice_ = driver.FindElement(By.CssSelector(".price-wrapper")).GetProperty("innerText").ToString();
+                Assert.AreEqual(duckname, header); //task 10.a
+                Assert.AreEqual(allprice, allprice_); //task 10.б
+                driver.Navigate().Back();
+                ducks = driver.FindElements(By.CssSelector("li a.link"));
+            }
+
+        }
+        [Test, Order(7)]
+        public void Test_7()
+        {
+            driver.Url = "http://localhost/litecart/";
+            var ducks = driver.FindElements(By.CssSelector("li s.regular-price"));
+            for (var j = 0; j < ducks.Count(); j++)
+            {
+                var regularpricecolor = ducks[j].GetCssValue("color");
+                var textdecoration = ducks[j].GetCssValue("text-decoration-line");
+                var regularsize = ducks[j].GetCssValue("font-size").Replace("px", "").Trim();
+                var regularsize_ = Convert.ToDecimal(regularsize,new CultureInfo("en-US"));
+
+                var saleprice = driver.FindElement(By.CssSelector(".campaign-price"));
+                var salepricecolor = saleprice.GetCssValue("color");
+                var salepricebold = saleprice.GetCssValue("font-weight");
+                var salepricesize =saleprice.GetCssValue("font-size").Replace("px", "").Trim();
+                var salepricesize_= Convert.ToDecimal(salepricesize,new CultureInfo("en-US"));
+
+                Assert.AreEqual(regularpricecolor, "rgba(119, 119, 119, 1)");//task 10.в
+                Assert.AreEqual(textdecoration, "line-through");//task 10.в
+                Assert.AreEqual(salepricecolor, "rgba(204, 0, 0, 1)");//task 10.г
+                Assert.AreEqual(salepricebold, "700");//task 10.г
+                if (regularsize_ < salepricesize_)
+                {
+                    Assert.IsTrue(true); //task 10.д
+                }
+                else throw new Exception("Акционная цена не крупнее обычной!");
+
+                ducks[j].Click();
+                wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.CssSelector("h1"))));
+
+                var regularpricecolor_ = driver.FindElement(By.CssSelector(".regular-price")).GetCssValue("color");
+                var textdecoration_ = driver.FindElement(By.CssSelector(".regular-price")).GetCssValue("text-decoration-line");
+                var salepricecolor_ = driver.FindElement(By.CssSelector(".campaign-price")).GetCssValue("color");
+                var salepricebold_ = driver.FindElement(By.CssSelector(".campaign-price")).GetCssValue("font-weight");
+                var regularsizenew = driver.FindElement(By.CssSelector(".regular-price")).GetCssValue("font-size").Replace("px", "").Trim();
+                var regularsizenew_ = Convert.ToDecimal(regularsizenew, new CultureInfo("en-US"));
+                var salepricesizenew = driver.FindElement(By.CssSelector(".campaign-price")).GetCssValue("font-size").Replace("px", "").Trim();
+                var salepricesizenew_ = Convert.ToDecimal(salepricesizenew, new CultureInfo("en-US"));
+
+                Assert.AreEqual(regularpricecolor_, "rgba(102, 102, 102, 1)");//task 10.в
+                Assert.AreEqual(textdecoration_, "line-through");//task 10.в
+                Assert.AreEqual(salepricecolor_, "rgba(204, 0, 0, 1)");//task 10.г
+                Assert.AreEqual(salepricebold_, "700");//task 10.г
+                if (regularsizenew_ < salepricesizenew_)
+                {
+                    Assert.IsTrue(true); //task 10.д
+                }
+                else throw new Exception("Акционная цена не крупнее обычной!");
+                driver.Navigate().Back();
+                ducks = driver.FindElements(By.CssSelector("li s.regular-price"));
+
             }
         }
     }
